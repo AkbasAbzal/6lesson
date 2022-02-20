fa491079736982e64530d1325f29477a957f2934
AkbasAbzal <77288564+AkbasAbzal@users.noreply.github.com>
2022-02-20 13:43:59 +0600

    Add files via upload

---
+++ 6lesson/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _6lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var procList = Process.GetProcesses();
+            foreach (var proc in procList)
+            {
+                Console.WriteLine($"{proc.Id} {proc.ProcessName}");
+            }
+            Console.Write("name = ");
+            var name = Console.ReadLine();
+            try
+            {
+                procList.First(p => p.ProcessName.ToLower() == name.ToLower()).Kill();
+                Console.WriteLine($"{name} deleted!");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine($"Process {name} not found!");
+            }
+        }
+    }
+}
